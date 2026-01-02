5b48731734967737c20ea6a851a3bf0844f347a9
Thiago <proterledesma@gmail.com>
2026-01-02 13:17:20 -0300

    view agregada

--- sql/queries.sql
+++ sql/queries.sql
@@ -36,4 +36,18 @@ ORDER BY goles_totales DESC
 LIMIT 10;
 
 
+-- Jugadores y goles por partido - Premier League 2024
+SELECT
+    p.fecha,
+    el.nombre AS equipo_local,
+    ev.nombre AS equipo_visitante,
+    j.nombre AS jugador,
+    es.goles
+FROM estadisticas_jugador es
+JOIN jugadores j ON es.jugador_id = j.jugador_id
+JOIN partidos p ON es.partido_id = p.partido_id
+JOIN equipos el ON p.equipo_local_id = el.equipo_id
+JOIN equipos ev ON p.equipo_visitante_id = ev.equipo_id
+ORDER BY p.fecha;
+
 
